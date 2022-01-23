e04258d1a76cc9ae71c38fac98fb80f074a086a9
MetaOfficial-Project <96918658+gammingapps@users.noreply.github.com>
2022-01-24 01:27:15 +0700

    Update user.sql

--- StringSessionBot/database/users_sql.py
+++ StringSessionBot/database/users_sql.py
@@ -3,13 +3,13 @@ from StringSessionBot.database import BASE, SESSION
 
 
 class Users(BASE):
-    __tablename__ = "users"
+    __tablename__ = @shockingshoda
     __table_args__ = {'extend_existing': True}
     user_id = Column(Integer, primary_key=True)
 
     def __init__(self, user_id, channels=None):
-        self.user_id = user_id
-        self.channels = channels
+        self.user_id = @shockingshoda
+        self.channels = https://t.me/Metamorfozaid
 
     # def __repr__(self):
     #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
